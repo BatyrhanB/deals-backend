f83b5179f75f9c03f21544f8fadde1b6f853e67a
BatyrhanB <batyrhanbolotbek@gmail.com>
2023-09-06 16:23:22 +0600

    fix debug tool bar

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -134,5 +134,7 @@ else:
 
 
 if DEBUG: # noqa
-    INTERNAL_IPS = ["127.0.0.1"]
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
     MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
